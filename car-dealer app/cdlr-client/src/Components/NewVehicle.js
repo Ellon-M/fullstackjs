4ca49243cbac99f8dca8cd9559dd34740fe86699
Ellon <mordecaimwema11@gmail.com>
2021-06-20 22:41:23 +0300

    ongoing

--- car-dealer app/cdlr-client/src/Components/NewVehicle.js
+++ car-dealer app/cdlr-client/src/Components/NewVehicle.js
@@ -14,25 +14,43 @@ const NewVehicle = () => {
     //     }
     // },[data])
 
-    const [price, setPrice] = useState(null);
-    const [brandName, setBrandName] = useState(null);
+    const [price, setPrice] = useState([]);
+    const [brandName, setBrandName] = useState([]);
     const [url, setUrl] = useState([]);
-    const [carData, setCarData] = useState([]);
+    const [key, setKey] = useState([]);
+    const [data, setData] = useState(null);
+    
+
+
+    const ref = firebase.database().ref("/newVehicle");
 
     useEffect(() => {
-        const ref = firebase.database().ref("/newVehicle");
-
-        ref.on('value', (snapshot) => {
-            console.log(snapshot.val());
+        ref.orderByKey().on('value', (snapshot) => {
+            const vehicle = snapshot.val();
+            // setBrandName(prevState => [...prevState, vehicle.brandName]);
+            // setPrice(prevState => [...prevState, vehicle.Price]);
+            // setUrl(prevState => [...prevState, vehicle.url]);
+            // setKey(prevState => [...prevState, snapshot.key])
+            // console.log(snapshot.val());
+            setData(vehicle);
           }, (errorObject) => {
             console.log('The read failed: ' + errorObject.name);
-          }); 
+          });
+    }, []);
 
-    }, [])
-
+    // console.log(brandName);
+    // console.log(price);
+    // console.log(key);
+    console.log(data);
     return ( <div>
-       
+      {/* {brandName.map(brand => (
+        <div>{brand}</div>
+      ))}
+           {price.map(price => (
+        <div>{price}</div> */}
+
     </div> );
 }
+
  
 export default NewVehicle;
